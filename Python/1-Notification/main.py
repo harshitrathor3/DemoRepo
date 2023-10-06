9e05d86ff53c5fda7f5819fa3f448b0b186adda3
Harshit Rathore <harshitrathorelink@gmail.com>
2023-10-06 11:59:57 +0530

    Initial Commit

---
+++ Python/1-Notification/main.py
@@ -0,0 +1,13 @@
+from notifypy import Notify
+
+notification = Notify()
+
+notification.title = "Notification Title"
+notification.message = "Notification Message"
+
+notification.send()
+
+print('Done')
+print('Om Namah Shivay')
+print('Jai Saraswati Mata')
+print('Jai Ranjeet Baba')
---
+++ Python/2-Battery/README.md
@@ -0,0 +1,11 @@
+# Python Battery Percentage
+
+Link of this repository:
+https://virgool.io/@BlackIQ/battery-percentage-paedt4ngepvx
+
+Link in Telegram Channel:
+https://t.me/Amir_Net/80
+
+Jai Shree Ram :)
+Jor se bolo
+"Jai Mata Di" !!
